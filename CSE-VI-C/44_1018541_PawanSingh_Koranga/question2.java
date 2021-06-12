7d6afb41eb1e392a93fb845ae7dab91f4a0cf611
PawanSinghKoranga <54844508+PawanSinghKoranga@users.noreply.github.com>
2021-06-12 10:12:40 +0530

    Add files via upload

---
+++ CSE-VI-C/44_1018541_PawanSingh_Koranga/question2.java
@@ -0,0 +1,37 @@
+package com.java.jdbc;
+import java.sql.*;
+public class question2{
+	public static void main(String[] args) throws SQLException {
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		String sql11 = "INSERT INTO jdbc_tbl VALUES (3, 'C++', 'Denis', '2014','Laxmi')";
+		String sql12 = "INSERT INTO jdbc_tbl VALUES (4, 'python', 'Sumitra', '2002','dhanraj')";
+		Scanner s = new Scanner(System.in);
+        		num = s.nextInt();
+		String sql2 = "SELECT * FROM jdbc_tbl where accno=num";
+		try {
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase", "root" , "ritik@123");
+			myStmt = myConn.createStatement();
+		/Insert into table/
+			myStmt.executeUpdate(sql11);
+			myStmt.executeUpdate(sql12);
+		/Check Updated Table/
+			myRs = myStmt.executeQuery(sql2);
+			System.out.println();
+			System.out.println("accno" + "    " + "title" + "  " + "author"+ "   " + "edition" + "   "+ "publisher");	
+			while (myRs.next()) {
+				System.out.println(myRs.getString("accno") + "     " + myRs.getString("title") + "     " + myRs.getString("author") + "    " +
+				myRs.getString("edition")+  "         "+ "       "+ myRs.getString("publisher"));
+			}
+		}
+	catch (Exception exc) {
+		exc.printStackTrace();
+	}
+	finally {
+		myRs.close();
+		myStmt.close();
+		myConn.close();
+	}
+      }
+}
