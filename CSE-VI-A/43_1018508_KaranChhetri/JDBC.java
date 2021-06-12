b51d96cbb262396e793d2b96a229a136cb8c5733
Karan Chhetri <57825189+Karan3011@users.noreply.github.com>
2021-06-11 21:40:44 -0700

    Add files via upload

---
+++ CSE-VI-A/43_1018508_KaranChhetri/JDBC.java
@@ -0,0 +1,42 @@
+import jdbc.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.*;
+
+public class Q2 {
+public static void main(String[] args) throws IOException{
+    Connection con = null;
+    Statememt stat = null;
+    ResultSet res = null;
+
+    try{
+        con = DriverManager.getConnection("jdbc:mysql://localhost:8085/practDB","root","L@kshay123");
+        // datbase practicalDb table book
+        stat = con.createStatement();
+        res = stat.executeQuery("select * from book");
+        
+        //displaying full table
+        while(res.next()){
+            System.out.println("Accesion No\t" + "Title\t" + "Author\t" + "Edition\t" + "Publisher\t");
+            System.out.println(res.getString("AccessionNo") + res.getString("Title") + res.getString("Author") + res.getString("Edition") + res.getString("Publisher"));
+        }
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String query = "select * from book where AccessionNO =" + n;
+        res = stat.executeQuery(query);
+
+        while(res.next()){
+            System.out.println("Accesion No\t" + "Title\t" + "Author\t" + "Edition\t" + "Publisher\t");
+            System.out.println(res.getString("AccessionNo") + res.getString("Title") + res.getString("Author") + res.getString("Edition") + res.getString("Publisher"));
+        }
+    }
+    catch(Exception exc){
+        exc.printStackTrace();
+    }
+
+
+}    
+}
