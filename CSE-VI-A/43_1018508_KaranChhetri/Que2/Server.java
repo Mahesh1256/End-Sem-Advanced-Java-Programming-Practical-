903237626105bb26d60c1b85b3dbbee2c6193e95
Karan Chhetri <57825189+Karan3011@users.noreply.github.com>
2021-06-11 21:34:34 -0700

    Add files via upload

---
+++ CSE-VI-A/43_1018508_KaranChhetri/Que2/Server.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+
+public class Server{
+	public static void main(String[] args) throws Exception{
+
+		ServerSocket ss=new ServerSocket(8000);//This is the server socket
+		Socket s=ss.accept(); //using accept() to establish connection.
+
+		System.out.println("connection is stable now ");//connection is stable here
+
+		
+		DataInputStream dataRead=new DataInputStream(s.getInputStream());//to input data from one device to another
+
+		DataOutputStream dataWrite=new DataOutputStream(s.getOutputStream());// to send data from this device to other
+
+		BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+
+		String str="",str2="";
+		while(!str.equals("bye")){	
+			str=dataRead.readUTF();//to read data from client
+			System.out.println("Client says: "+str);
+			str2=br.readLine();
+			dataWrite.writeUTF(str2);
+			dataWrite.flush();
+			}
+
+		dataRead.close();
+		s.close();
+		ss.close(); }}
