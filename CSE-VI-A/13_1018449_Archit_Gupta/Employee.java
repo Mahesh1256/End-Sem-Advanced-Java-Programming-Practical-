f7d2820261779b210cc44e77570c521f81462ead
ArchitGupta8080 <79796520+ArchitGupta8080@users.noreply.github.com>
2021-06-12 10:13:19 +0530

    Add files via upload

---
+++ CSE-VI-A/13_1018449_Archit_Gupta/Employee.java
@@ -0,0 +1,38 @@
+package ques3;
+//Archit Gupta 1018449
+import com.opensymphony.xwork2.ActionSupport;
+@SuppressWarnings("serial")
+public class Employee extends ActionSupport{
+ private String name;
+ private int age;
+
+ public String execute() {
+ return "success";
+ }
+//Archit Gupta 1018449
+ public void validate() {
+ if(name==null || name.trim().equals("")) {
+ addFieldError("name","The name is required");
+ }
+ if(age<28 || age>65)
+ {
+ addFieldError("age","Age must be in between 28 and 65");
+ }
+ }
+ //Archit Gupta 1018449
+ public String getName() { 
+ return name;
+ }
+ public void setName(String name) {
+ this.name = name;
+ }
+ public int getAge() {
+ return age;
+ }
+ public void setAge(int age) {
+ this.age = age;
+ }
+
+
+} 
+//Archit Gupta 1018449
