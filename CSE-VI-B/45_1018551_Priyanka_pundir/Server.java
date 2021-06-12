e3eabed6d1fbfbcb2be549639613b70c3adeb936
priyanka_pundir <55492090+pundir000@users.noreply.github.com>
2021-06-12 10:54:45 +0530

    Update Server.java

--- CSE-VI-B/45_1018551_Priyanka_pundir/Server.java
+++ CSE-VI-B/45_1018551_Priyanka_pundir/Server.java
@@ -1,6 +1,6 @@
 import java.net.*;
 import java.io.*;
-public class q2server{
+public class Server{
 public static void main(String args[])throws Exception
 {
 ServerSocket ss= new ServerSocket(8080);
