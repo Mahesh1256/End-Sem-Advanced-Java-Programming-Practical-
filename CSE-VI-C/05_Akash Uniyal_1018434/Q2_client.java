75a4903c6c6ea802f9fd12521ac5c5b77d0a7c1f
Akash-uniyal <85742737+Akash-uniyal@users.noreply.github.com>
2021-06-11 23:47:19 -0500

    Add files via upload

---
+++ CSE-VI-C/05_Akash Uniyal_1018434/Q2_client.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+public class client {
+    public static void main(String[] args)throws Exception{
+        
+            Socket s = new Socket("localhost",8080);
+
+            DataInputStream dataRead = new DataInputStream(s.getInputStream());
+            DataOutputStream dataWrite = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String str="",str2="";
+            String pp="";String gg="";
+            while(!str.equals("Stay Safe")){
+                str = br.readLine();
+                dataWrite.writeUTF(str);
+                dataWrite.flush();
+                str2 = dataRead.readUTF();
+                System.out.println("server says : " +str2);
+            }
+            dataRead.close();
+            s.close();
+    }
+}
