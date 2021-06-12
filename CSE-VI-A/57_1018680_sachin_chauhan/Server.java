701248af9a808e1e665460967f3ad6c4b08a7570
sachin1197 <85725212+sachin1197@users.noreply.github.com>
2021-06-12 11:00:06 +0530

    Add files via upload

---
+++ CSE-VI-A/57_1018680_sachin_chauhan/Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+	
+	private static int port = 9090;
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			ServerSocket server = new ServerSocket(port);
+			
+			Socket client = server.accept();
+			
+			DataInputStream dataReader = new DataInputStream(client.getInputStream());
+			
+			DataOutputStream dataWriter = new DataOutputStream(client.getOutputStream());
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+			String recvMsg = "";
+			
+			String sendMsg = "";
+			
+			while(!recvMsg.equalsIgnoreCase("bye")) {
+				
+				recvMsg = dataReader.readUTF();
+				
+				System.out.println("client says: " + recvMsg);
+
+				sendMsg = reader.readLine();
+				
+				dataWriter.writeUTF(sendMsg);
+				
+				dataWriter.flush();
+			}
+
+			dataReader.close();
+			client.close();
+			server.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
