1c4a4b87e51f6a312886140631bb5f41d384efa9
HRITIK SAINI <69029275+hritiksaini07@users.noreply.github.com>
2021-06-12 09:25:27 +0530

    Update Client.java

--- CSE-VI-A/40_1018502_Hritik_Saini/Q2/Client.java
+++ CSE-VI-A/40_1018502_Hritik_Saini/Q2/Client.java
@@ -5,18 +5,18 @@ public static void main(String args[])throws Exception{
 
 Socket s= new Socket("localhost",8080);		//to get the server connection	
  
-DataInputStream dataRead =new DataInputStream(s.getInputStream());
+DataInputStream input =new DataInputStream(s.getInputStream());
 
-DataOutputStream dataWrite = new DataOutputStream(s.getOutputStream());
+DataOutputStream output = new DataOutputStream(s.getOutputStream());
 
 BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
 String str="", str2="";
 while(!str.equals("bye")){
 str=br.readLine();
-dataWrite.writeUTF(str);
-dataWrite.flush();
-str2 = dataRead.readUTF(); 			 //to read message from server
+output.writeUTF(str);
+output.flush();
+str2 = input.readUTF(); 			 //to read message from server
 System.out.println("Server Says:-  "+str2);  	//to print message received from server
 }
 dataRead.close();
