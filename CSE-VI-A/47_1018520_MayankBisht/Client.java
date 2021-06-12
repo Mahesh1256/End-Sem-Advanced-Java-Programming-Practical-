b9ecc77c560b19bbe7e8e9d2a58f8c1aa8a21e9f
Mayank11111 <78432753+Mayank11111@users.noreply.github.com>
2021-06-12 09:43:16 +0530

    Add files via upload

---
+++ CSE-VI-A/47_1018520_MayankBisht/Client.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+public class Client{
+public static void main(String args[])throws Exception{
+
+Socket s= new Socket("localhost",8080);
+
+DataInputStream dataRead =new DataInputStream(s.getInputStream());
+
+DataOutputStream dataWrite = new DataOutputStream(s.getOutputStream());
+
+BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+String str="", str2="";
+while(!str.equals("bye")){
+str=br.readLine();
+dataWrite.writeUTF(str);
+dataWrite.flush();
+str2 = dataRead.readUTF();
+System.out.println("Server Says:-  "+str2);
+}
+dataRead.close();
+s.close();
+}}
