68c0fcf9af63af90b99491d372dc263fd5cc3770
AkhilBarthwal005 <67703904+AkhilBarthwal005@users.noreply.github.com>
2021-06-12 09:59:49 +0530

    question 2 uploded

---
+++ CSE-VI-C/06_1018435_Akhil_Barthwal/q2/ClientQuestion.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.*;
+import java.net.*;
+//Name - Akhil Barthwal
+//        Rollno - 06
+//        section - C
+//        University Rollno - 1018435
+public class ClientQuestion{
+    public static void main(String args[])throws Exception{
+
+        Socket s=new Socket("localhost",9090);
+
+        DataInputStream dataRead=new DataInputStream(s.getInputStream());
+
+        DataOutputStream dataWrite=new DataOutputStream(s.getOutputStream());
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+
+        while(!str.equals("Bye")){
+            str=br.readLine();
+            dataWrite.writeUTF(str);
+            dataWrite.flush();
+            str2=dataRead.readUTF();
+            System.out.println("Server Says: "+str2);
+        }
+
+        dataRead.close();
+        s.close();
+    }
+}
