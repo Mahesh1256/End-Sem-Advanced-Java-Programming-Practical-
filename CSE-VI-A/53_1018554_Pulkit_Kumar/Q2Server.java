05aa2115c2cbbed831a8d8d7538edd1741e8b222
liegepulkit <56088495+liegepulkit@users.noreply.github.com>
2021-06-12 10:05:30 +0530

    Add files via upload

---
+++ CSE-VI-A/53_1018554_Pulkit_Kumar/Q2Server.java
@@ -0,0 +1,25 @@
+
+import java.net.*;
+import java.io.*;
+public class Server{
+ public static void main(String[] args) throws Exception{
+ ServerSocket ss=new ServerSocket(8080);
+ Socket s=ss.accept();
+ System.out.println("connection is stable now ");
+ DataInputStream dataRead=new DataInputStream(s.getInputStream());
+ DataOutputStream dataWrite=new DataOutputStream(s.getOutputStream());
+ BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+ String str="",str2="";
+ while(!str.equals("bye")){
+ str=dataRead.readUTF();//reads client Message
+ System.out.println("Client says: "+str);
+ str2=br.readLine();
+ dataWrite.writeUTF(str2);
+ dataWrite.flush();
+ }
+ dataRead.close();
+ s.close();
+ ss.close();
+ }
+}
+//53_1018554_Pulkit_Kumar 
