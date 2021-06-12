0adae136c2221d45b28ee028503589e606bc75d1
aniluyal97153 <52264327+aniluyal97153@users.noreply.github.com>
2021-06-12 10:18:55 +0530

    Add files via upload

---
+++ CSE-VI-A/7_1018441_Anil_Singh/Question2_Client.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+public class Question2_Client
+	{
+	public static void main(String[] args) throws Exception 
+	{
+	Socket s=new Socket("localhost",8081);
+	DataInputStream dataRead=new DataInputStream(s.getInputStream());
+	DataOutputStream dataWrite=new DataOutputStream(s.getOutputStream());
+	BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+	String str="",str2="";
+	while(!str.equals("bye")){
+		str=br.readLine();
+		dataWrite.writeUTF(str);
+		dataWrite.flush();
+		str2=dataRead.readUTF();
+		System.out.println("Server says: "+str2);
+	}
+	dataRead.close();
+	s.close();
+	} }
