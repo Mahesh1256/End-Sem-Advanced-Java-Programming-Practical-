5c458e5bbc6a23d7dc2913b724646373027450f1
Arvind Singh <42924155+singharvindbisht@users.noreply.github.com>
2021-06-12 10:08:03 +0530

    Add files via upload

---
+++ CSE-VI-A/15_1018451_Arvind_Singh/Q2/Client.java
@@ -0,0 +1,28 @@
+//Name: Arvind Singh
+//rollno: 1018451
+import java.net.*;
+import java.io.*;
+public class client{
+	public static void main(String args[])
+	throws Exception{
+		Socket s= new Socket("localhost",8080);
+		//dataRead
+		DataInputStream dataRead = new DataInputStream(s.getInputStream());
+		//dataWrite
+		DataOutputStream dataWrite = new DataOutputStream(s.getOutputStream());
+		//keyboard Reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str="",str2="";
+		while(!str.equals("bye"))
+		{
+			str=br.readLine();
+			dataWrite.writeUTF(str);
+			dataWrite.flush();
+			str2=dataRead.readUTF();
+			System.out.println("server says :"+str2);
+		}
+		dataRead.close();
+		s.close();
+	}
+}
